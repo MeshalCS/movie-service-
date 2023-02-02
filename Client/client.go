e797a2f74bb11ba6546ed675e94d6e9a6d450a34
MeshalCS <80965738+MeshalCS@users.noreply.github.com>
2023-02-02 18:56:34 +0300

    Add files via upload

---
+++ Client/client.go
@@ -0,0 +1,24 @@
+// Alert: This code is in progress to implement requirements 
+// ********************************************************	
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	//pb "movie" // Import the generated protobuf code
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	address := "localhost:50051" // Address of the gRPC service
+
+	// Connect to the gRPC service
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	
+
+// ********************************************************	
+// Alert: This code is in progress to implement requirements 
