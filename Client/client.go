e614ff337229dcab0bea3f8f14a57d7d7fe68f4f
MeshalCS <80965738+MeshalCS@users.noreply.github.com>
2023-02-04 10:45:18 +0300

    Update client.go

--- Client/client.go
+++ Client/client.go
@@ -25,7 +25,23 @@ func main() {
 
 	// Create a client using the connection
 	client := pb.NewMovieServiceClient(conn)
+	
+
 
 }
+
+//fun LatestMovies for the user want to see the last movies
+func GetLatestMovies(client pb.MovieServiceClient) string {
+	// Call the LatestMovies method
+	latestMoviesResponse, err := client.LatestMovies(context.Background(), &pb.LatestMoviesRequest{})
+	fmt.Println("Latest Movies:")
+	for _, movie := range latestMoviesResponse.Movies {
+		fmt.Println("-", movie.Name)
+	}
+}
+
+
+
+
 // ********************************************************	
 // Alert: This code is in progress to implement requirements 
